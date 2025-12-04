5e4c92d82efac92e87c4b785342ec09d0a257631
Ariel Acosta <j.ariel.acosta17@gmail.com>
2025-12-04 17:05:25 -0500

    feat: modify  first initial test to cover only user registration

--- tests/api_tests/test_registration.py
+++ tests/api_tests/test_registration.py
@@ -5,17 +5,26 @@ from screenplay.tasks.login_user import LoginUser
 from screenplay.questions.response_status import ResponseStatus
 from screenplay.assertions.see_that import SeeThat
 from screenplay.assertions.matchers import equals_to
+from uuid import uuid4
 
 BASE_URL ="http://localhost:8000"
 
 def test_user_can_login():
     actor = Actor("Ariel").can(CallAPI(BASE_URL))
 
+    username = f"ariel_{uuid4().hex[:6]}"
+    email = f"{username}@example.com"
+
     actor.attempts_to(
-        RegisterUser({"user": {"username": "ariel"}, "email": "ariel@example.com", "password":"123456"}),
-        LoginUser({"user":{"email":"ariel@example.com", "password":"123456"}})
+        RegisterUser({
+            "user": {
+                "username": username,
+                "email": email, 
+                "password":"123456"
+                }
+            })
     )
 
     actor.should(
-        SeeThat(ResponseStatus(), equals_to(200))
+        SeeThat(ResponseStatus(), equals_to(201))
     )

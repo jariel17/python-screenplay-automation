8aee31b3d33f1c6054487a7befaddf4ef3758839
Ariel Acosta <j.ariel.acosta17@gmail.com>
2025-12-10 17:20:44 -0500

    improvements: -Add logger to the actor an ability to remember token. -Rename registration test to match intention

--- screenplay/tasks/login_user.py
+++ screenplay/tasks/login_user.py
@@ -1,5 +1,9 @@
+import logging
+
 from screenplay.abilities.call_api import CallAPI
 from screenplay.tasks.base_task import Task
+
+logger = logging.getLogger(__name__)
 
 
 class LoginUser(Task):
@@ -10,4 +14,9 @@ class LoginUser(Task):
         api = actor.ability_to(CallAPI)
         response = api.post("/api/users/login", json=self.credentials)
         actor.remember("last_response", response)
+        try:
+            token = response.json()["user"]["token"]
+            actor.remember("token", token)
+        except KeyError:
+            logger.warning("LoginUser: token not found in response")
         return response
